cfc9cba66f5cc50da2656376a9741d69c074384b
liana16 <liana_you@bk.ru>
2022-12-04 15:54:23 +0300

    Задача 4

---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число
+//  и выдает количество цифр в числе
+// 456 ->3
+
+int Count(int A)
+{
+    int i = 0;
+    while(i < A)
+    {
+        A = A / 10;
+        i++;
+    }
+    return i;
+}
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int answer = Count(x);
+Console.WriteLine(answer);

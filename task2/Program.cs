c8dbc8c76ecf7eba059b8402cc4e82df46462190
liana16 <liana_you@bk.ru>
2022-12-04 15:56:13 +0300

    Задача 2

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿int SayNum(int num1, int num2)
+{
+    if (num1 > num2)
+    {
+        return num1;
+    }
+    else
+    {
+        return num2;
+    }
+}
+
+int x = SayNum(2,3);
+Console.WriteLine(x);
+
+Console.WriteLine(SayNum(7, 5));
+
+int x1 = 10;
+int x2 = 17;
+int x3 = SayNum(x1, x2);
+Console.WriteLine(x3);

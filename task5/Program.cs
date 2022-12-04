b1fa861e0ead34cd4cd71d54b17923889633190b
liana16 <liana_you@bk.ru>
2022-12-04 15:53:10 +0300

    Задача 5

---
+++ task5/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу, которая принимает на вход число N 
+// и выдает произведение чисел от 1 до N
+// 4->24
+
+int Count(int N)
+{
+    int prz = 1;
+    int i = 1;
+    while(i <= N)
+    {
+        prz = prz * i;
+        i++;
+    }
+    return prz;
+}
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int answer = Count(x);
+Console.WriteLine(answer);
